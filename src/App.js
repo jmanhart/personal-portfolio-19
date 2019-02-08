9b201fbb32d22a69b3824381d0ad7821de897d6c
Manhart, John <manhartjohn@gmail.com>
2019-02-07 21:20:04 -0600

    Hmmm what to do?

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+const propTypes = {
+  children: PropTypes.element.isRequired,
+  routes: PropTypes.array.isRequired,
+};
 
-import Home from './containers/home/index.js';
-import About from './containers/about/index.js';
-const BasicExample = () => (
-  <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-
-      <hr />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </div>
-  </Router>
-);
-
-
-
-
-
-
-export default BasicExample;
+export default class App extends Component{
+    render(){
+        return(
+          <div>
+            <h1>WHY YOU NO WORK?</h1>
+              {this.props.children}
+          </div>
+        )
+    }
+}
+App.propTypes = propTypes;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// Importing Components
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Home from './containers/home/index.js';
+import About from './containers/about/index.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Importing Styles
+import './index.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const routes = () => (
+    <Route path="/" component={App}>
+        <Route exact path="/home" component={Home} />
+        <Route path="/about" component={About} />
+    </Route>
+);
+
+ReactDOM.render(
+    <Router
+        routes={routes} 
+    />, 
+    document.getElementById('root')
+);
+
+
