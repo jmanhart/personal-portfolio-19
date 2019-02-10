be82cab25f99c81fa9dedf60bc4e9d4a116991f1
Manhart, John <manhartjohn@gmail.com>
2019-02-09 21:45:07 -0600

    building a new homepage and grid have it working on Desktop and mobile

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+// import styles
+import './../../App.scss';
+
 export default class About extends Component{
     render(){
         return(
--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
+
+// import styles
+import './../../App.scss';
+
 export default class Home extends Component{
     render(){
         return(
-          <div>
-              <h1>This is the the home page</h1>
+          <div className="container">
+            <div id="header"><h1>Hello, I'm John!</h1></div>
+            <div id="about">
+                <h2>A Designer & Developer living in Kansas City and working for Garmin.</h2>
+                <h3>Lorem ipsum dolor amet vice banh mi aesthetic authentic, small batch truffaut farm-to-table sriracha squid post-ironic actually af. Chambray direct trade meditation fixie 3.</h3>
+            </div>
+            <ul className="caseStudies">
+            <h3>My Work</h3>
+                <li><div></div></li>
+                <li><div></div></li>
+                <li><div></div></li>
+                <li><div></div></li>
+                <li><div></div></li>
+            </ul>
           </div>
+          
         )
     }
 }
