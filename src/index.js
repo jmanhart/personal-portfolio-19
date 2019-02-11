441e2a20c34f9bc792284ae4aa6cb621a106e9bc
Manhart, John <manhartjohn@gmail.com>
2019-02-10 19:27:07 -0600

    updates after updates everything is good yes awesone

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import ScrollToTop from 'react-router-scroll-top'
+
 
 // Importing in Components
 import App from './App';
 
 render((
     <BrowserRouter >
-      <ScrollToTop>
+
       <App />
-      </ScrollToTop>
+
     </BrowserRouter>
 ), document.getElementById('root'));
 
