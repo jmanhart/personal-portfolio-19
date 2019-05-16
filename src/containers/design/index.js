172c2265477d78a28f4ae5ae04d2744ec690dba2
Manhart, John <manhartjohn@gmail.com>
2019-05-16 13:57:04 -0500

    Adding in more work to the design Page

--- src/containers/design/index.js
+++ src/containers/design/index.js
@@ -29,13 +29,36 @@ export default class Design extends Component{
 
                     <h3 className="header-type-01">Font Hierarchy</h3>
                     <ul className="font-size">
-                        <li><h1>H1</h1></li>
-                        <li><h2>H2</h2></li>
-                        <li><h3>H3</h3></li>
-                        <li><h4>H4</h4></li>
-                        <li><h5>H5</h5></li>
+                        <li><h1>H1 - Lorem ipsum dolor amet sartorial taxidermy swag, hot chicken actually slow-carb artisan.</h1></li>
+                        <li><h2>H2 - Lorem ipsum dolor amet sartorial taxidermy swag, hot chicken actually slow-carb artisan.</h2></li>
+                        <li><h3>H3 - Lorem ipsum dolor amet sartorial taxidermy swag, hot chicken actually slow-carb artisan.</h3></li>
+                        <li><h4>H4 - Lorem ipsum dolor amet sartorial taxidermy swag, hot chicken actually slow-carb artisan.</h4></li>
+                        <li><h5>H5 - Lorem ipsum dolor amet sartorial taxidermy swag, hot chicken actually slow-carb artisan.</h5></li>
                         <li><p>p - Lorem ipsum dolor amet sartorial taxidermy swag, hot chicken actually slow-carb artisan 3 wolf moon celiac man braid four loko before they sold out shabby chic. Knausgaard woke flannel prism hot chicken put a bird on it. Master cleanse lumbersexual messenger bag vexillologist. Echo park hot chicken neutra, cold-pressed sustainable leggings +1 tofu dreamcatcher. Disrupt stumptown gastropub locavore art party. Direct trade listicle selvge, gentrify tilde wolf ethical distillery. Dreamcatcher artisan activated charcoal.</p></li>
                     </ul>        
+
+                    <h3 className="header-type-01">Layout Scale</h3>
+                    <ul className="layout-scale">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul> 
+
+                    <h3 className="header-type-01">Spacing Scale</h3>
+                    <ul className="spacing-scale">
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+    
+                    </ul> 
 
                 </div>
             </>
