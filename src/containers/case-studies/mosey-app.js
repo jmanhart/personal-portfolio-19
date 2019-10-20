5da13486dbebc010ed7a09d2754ee1d52c237907
jmanhart <jmanhart@sentry.io>
2019-10-20 16:18:34 -0500

    Adding in some code refactoring need to finish this shit...

--- src/containers/case-studies/mosey-app.js
+++ src/containers/case-studies/mosey-app.js
@@ -23,30 +23,35 @@ const arrOrder = 1
 export default class MoseyApp extends Component{
     render(){
         return(
-            <>
-            <ButtonBack buttonLabel="Back" linkToPath="/" />
-
-            <BasicContainer>
-                <div id="header">
-                    <h1>{data.casestudies[arrOrder].title}</h1>
-                    <h2>{data.casestudies[arrOrder].descriptionshort}</h2>
-                    <h3>{data.casestudies[arrOrder].details}</h3>
-                </div>
-            </BasicContainer>
-
-            <ImageContainerWrapper />
-           
-            <BasicContainer>
-                <div id="about">
-                    <p>Lorem ipsum dolor amet sartorial taxidermy swag, hot chicken actually slow-carb artisan 3 wolf moon celiac man braid four loko before they sold out shabby chic. Knausgaard woke flannel prism hot chicken put a bird on it. Master cleanse lumbersexual messenger bag vexillologist. Echo park hot chicken neutra, cold-pressed sustainable leggings +1 tofu dreamcatcher. Disrupt stumptown gastropub locavore art party.
-                    </p>
-                    <p>Direct trade listicle selvge, gentrify tilde wolf ethical distillery. Dreamcatcher artisan activated charcoal disrupt church-key, stumptown fashion axe street art. Letterpress salvia trust fund semiotics four loko. Celiac keytar leggings polaroid copper mug, fixie chambray pour-over hell of photo booth. Wolf hashtag truffaut kinfolk art party banjo. Leggings iceland poke hot chicken, lumbersexual pok pok sartorial trust fund mixtape helvetica crucifix la croix umami.
-                    </p>
-                    <p>Authentic hell of mixtape thundercats, readymade tofu tattooed brooklyn. Retro seitan glossier wayfarers keffiyeh narwhal hoodie tousled vexillologist shaman poutine gochujang forage readymade. Kombucha blog hexagon squid shoreditch thundercats beard single-origin coffee keytar butcher polaroid. Tattooed food truck squid beard microdosing humblebrag brunch XOXO 8-bit keffiyeh cornhole.
-                    </p>
-                </div>
-            </BasicContainer>
-            </>
+            <PageWrapper>
+                <ButtonBack buttonLabel="Back" linkToPath="/" />
+                <BasicContainer>
+                    <div id="header">
+                        <h1>{data.casestudies[arrOrder].title}</h1>
+                        <h2>{data.casestudies[arrOrder].descriptionshort}</h2>
+                        <h3>{data.casestudies[arrOrder].details}</h3>
+                    </div>
+                </BasicContainer>
+                <ImageContainerWrapper />
+                <BasicContainer>
+                    <div id="about">
+                        <p>Lorem ipsum dolor amet sartorial taxidermy swag, hot chicken actually slow-carb artisan 3 wolf moon celiac man braid four loko before they sold out shabby chic. Knausgaard woke flannel prism hot chicken put a bird on it. Master cleanse lumbersexual messenger bag vexillologist. Echo park hot chicken neutra, cold-pressed sustainable leggings +1 tofu dreamcatcher. Disrupt stumptown gastropub locavore art party.
+                        </p>
+                        <p>Direct trade listicle selvge, gentrify tilde wolf ethical distillery. Dreamcatcher artisan activated charcoal disrupt church-key, stumptown fashion axe street art. Letterpress salvia trust fund semiotics four loko. Celiac keytar leggings polaroid copper mug, fixie chambray pour-over hell of photo booth. Wolf hashtag truffaut kinfolk art party banjo. Leggings iceland poke hot chicken, lumbersexual pok pok sartorial trust fund mixtape helvetica crucifix la croix umami.
+                        </p>
+                        <p>Authentic hell of mixtape thundercats, readymade tofu tattooed brooklyn. Retro seitan glossier wayfarers keffiyeh narwhal hoodie tousled vexillologist shaman poutine gochujang forage readymade. Kombucha blog hexagon squid shoreditch thundercats beard single-origin coffee keytar butcher polaroid. Tattooed food truck squid beard microdosing humblebrag brunch XOXO 8-bit keffiyeh cornhole.
+                        </p>
+                    </div>
+                </BasicContainer>
+            </PageWrapper>
         )
     }
 }
+
+const PageWrapper = styled('div')`
+    box-sizing: border-box;
+    margin: 10rem auto;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+`
